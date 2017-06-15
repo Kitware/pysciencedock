6c8883f1a5bf069e4190b37f1612824b8579c015
Jeffrey Baumes <jeff.baumes@kitware.com>
2017-06-15 09:37:36 -0400

    preserve output filenames

--- pysciencedock/describe.py
+++ pysciencedock/describe.py
@@ -34,7 +34,7 @@ class Description(object):
 
         arguments = [name]
         arguments += ['--%s=$input{%s}' % (x['id'], x['id']) for x in self._inputs]
-        arguments += ['--%s=/mnt/girder_worker/data/%s' % (x['id'], x.get('path', x['id'])) for x in self._outputs]
+        arguments += ['--%s=$output{%s}' % (x['id'], x['id']) for x in self._outputs]
         spec['container_args'] = arguments
 
         spec['docker_image'] = self._dockerImage
@@ -118,7 +118,8 @@ class Description(object):
             'id': id,
             'name': name,
             'description': description,
-            'type': type
+            'type': type,
+            'path': '$output{%s}' % id
         }
 
         if serialize is not None:
